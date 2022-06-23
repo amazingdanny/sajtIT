19a31b748ca1dd17c8225a891081ddc35d899d52
amazingdanny <blaskoff_2003@abv.bg>
2022-06-23 16:09:47 +0300

    dobawih lay out za produkti

---
+++ products.js
@@ -0,0 +1,36 @@
+const products = [ 
+    
+    {
+        id: 0,
+        name:"djapanka 1",
+        price: 9.99,
+        instock: 5,
+        description:"a",
+        imgSrc: "./img/"
+    },
+    {
+        id: 1,
+        name:"djapanka 2",
+        price: 9.99,
+        instock: 5,
+        description:"a",
+        imgSrc: "./img/"
+    },
+    {
+        id: 2,
+        name:"djapanka 3",
+        price: 9.99,
+        instock: 5,
+        description:"a",
+        imgSrc: "./img/"
+    },
+    {
+        id: 3,
+        name:"djapanka 4",
+        price: 9.99,
+        instock: 5,
+        description:"a",
+        imgSrc: "./img/"
+    },
+
+ ]
